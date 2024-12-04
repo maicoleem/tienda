e8d970187bb2ffd64563ba7dec4077cde26e0f22
loro <maicoleem@gmail.com>
2024-12-03 21:39:55 -0500

    filtro almacenamiento ok

--- static/almacenamiento.js
+++ static/almacenamiento.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
+const filtroBodega = document.getElementById('filtro-bodega');
+const filtroProducto = document.getElementById('filtro-producto');
+const tbody = document.getElementById('tabla-almacenamiento');
+
 const cargarAlmacenamiento = async() => {
     try{
         const response = await fetch('/api/almacenamiento/');
@@ -7,7 +11,6 @@ const cargarAlmacenamiento = async() => {
             throw new Error('Error al obtener registros: ${response.statusText}')
         }
         const almacenamientos = await response.json();
-        const tbody = document.getElementById('tabla-almacenamiento');
         tbody.innerHTML = '';
         almacenamientos.forEach(almacenamiento =>{
             const fila = document.createElement('tr');
@@ -29,6 +32,29 @@ const cargarAlmacenamiento = async() => {
         alert('Hubo un problema al cargar los almacenamientos. Consulte la consola para más detalles.')
     }
 }
+
+function filtrarTablaAlmacen() {
+    const filtrarBodega = filtroBodega.value.toLowerCase();
+    const filtrarProducto = filtroProducto.value.toLowerCase();
+
+    Array.from(tbody.children).forEach(li => {
+          // Separar el contenido por líneas
+          const lineas = li.textContent.split('\n').map(linea => linea.trim());
+          const bodega = lineas[1];
+          const producto = lineas[3];
+          li.style.display =
+              (bodega.toLowerCase().includes(filtrarBodega) &&
+               producto.toLowerCase().includes(filtrarProducto))
+                  ? ''
+                  : 'none';
+  
+          console.log(bodega, producto);
+    });
+}
+
+
+filtroBodega.addEventListener('input', filtrarTablaAlmacen)
+filtroProducto.addEventListener('input', filtrarTablaAlmacen)
 cargarAlmacenamiento()
 
 })
